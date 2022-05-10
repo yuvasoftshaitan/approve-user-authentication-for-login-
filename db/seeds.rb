70522be3f06fe6fa787e70c9135ea808250cd10a
shaitan <shaitanyuvasoft358@gmail.com>
2022-05-10 12:32:29 +0530

    local host cheng

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
      
+
+     User.new(email: "admin@gmail.com",password:123456, password_confirmation: 123456, typename:"manejar", usersinput:true).save
